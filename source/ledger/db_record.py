c103a91cc222d9e05aa0e7bc7db37509697a1573
mukund-riva-mumbai <mukund.dh@rivadigital.com>
2015-12-14 15:57:04 +0530

    Added the data generation for the INSERT INTO command; will be using the executemany convenience feature in MySQLdb to insert stuff into the database.

---
+++ source/ledger/db_record.py
@@ -0,0 +1,67 @@
+"""
+ db_record
+ Riva's database record insert command builder.
+ We will NOT be using MODIFY or DELETE records here; we don't want to do any of
+ these things in our database. Our database is going to be a purely "ADD/APPEND"
+ system.
+"""
+__author__ = "mdhananjay"
+
+
+def generate_insert_str(table_name="", param_list=[]):
+    """
+    Generates the INSERT INTO statement to put data into our database. Takes in a table name,
+    and the values for the rows in said table.
+    The param_list is a list of dictionaries, which hold the row:value lookup. Eg:
+    [{"firstname": "Mukund", "lastname": "Dhananjay"}, {"firstname": "Akesh", "lastname": "Kulmi"}]
+    This makes it convenient for the coders to input and verify the data they're sending in
+    :param table_name: str Name of the table to insert record into
+    :param param_list: list List of dictionaries containing rows and their values
+    :return: tuple The INSERT command paired with the list of the tuples containing the row values
+    """
+    if not table_name:
+        raise ValueError("table_name CANNOT BE NULL")
+
+    if not param_list:
+        raise ValueError("param_list CANNOT BE EMPTY")
+
+    if type(param_list[0])() != {}:
+        raise ValueError("param_list SHOULD CONTAIN ONLY DICTIONARIES")
+
+    # What we have to do here is establish a baseline for the number of rows
+    # we will expect. By default the first item found in the param_list will
+    # be treated as the "template" object, for the lack of a better term.
+    # If the rest of the items in the list have any discrepancies, like missing
+    # row names or a different row count, errors will be raised.
+    def_row_names = param_list[0].keys()
+    def_row_names.sort()
+    def_row_count = len(def_row_names)
+
+    values_list = []
+    for p_dict in param_list:
+        if len(p_dict.keys()) != def_row_count:
+            raise ValueError("Mismatch in row counts. Expected %d, got %d." % (def_row_count, len(p_dict.keys())))
+
+        row_names = p_dict.keys()
+        row_names.sort()
+
+        if row_names != def_row_names:
+            raise ValueError("Mismatch in the row names. Please check the value passed to param_list.")
+
+        values = []
+        for row in def_row_names:
+            values.append(p_dict.get(row))
+        values_list.append(tuple(values))
+
+    insert_str = "\"\"\"INSERT INTO %s VALUES (" % (table_name + " (" + ", ".join(def_row_names) + ")")
+    for i in xrange(0, def_row_count):
+        insert_str += "%s, "
+    insert_str = insert_str[:-2] + ")\"\"\""
+
+    return insert_str, values_list
+
+if __name__ == "__main__":
+    print generate_insert_str("department", [{"departmentname": "'layout'", "departmentcode": "'lay'"},
+                                             {"departmentname": "'lighting'", "departmentcode": "'lit'"},
+                                             {"departmentname": "'simulation'", "departmentcode": "'sim'"}])
+
